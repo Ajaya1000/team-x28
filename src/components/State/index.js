a9b0e4613aba8908d382e002b74c0c722829befe
Ajaya1000 <ajaymati1000@gmail.com>
2021-05-27 14:45:51 +0530

    added default value to state and gave background color to the selected

--- src/components/State/index.js
+++ src/components/State/index.js
@@ -1,27 +1,27 @@
-import { Grid, InputAdornment, Paper, TextField } from "@material-ui/core";
-import React, { useEffect, useState } from "react";
+import { Grid, InputAdornment, Paper, TextField } from '@material-ui/core';
+import React, { useEffect, useState } from 'react';
 // import HeaderCard from './HeaderCard';
-import StateCardNew from "../StateCardNew";
-import StateSelect from "../StateSelect";
-import SearchIcon from "@material-ui/icons/Search";
-import { makeStyles } from "@material-ui/core/styles";
+import StateCardNew from '../StateCardNew';
+import StateSelect from '../StateSelect';
+import SearchIcon from '@material-ui/icons/Search';
+import { makeStyles } from '@material-ui/core/styles';
 // import Paper from '@material-ui/core/Paper';
-import InputBase from "@material-ui/core/InputBase";
-import Divider from "@material-ui/core/Divider";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
+import InputBase from '@material-ui/core/InputBase';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 // import SearchIcon from '@material-ui/icons/Search';
-import DirectionsIcon from "@material-ui/icons/Directions";
-import Axios from "axios";
+import DirectionsIcon from '@material-ui/icons/Directions';
+import Axios from 'axios';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    padding: "2px 4px",
-    display: "flex",
-    alignItems: "center",
-    width: "90%",
-    backgroundColor: "#F0F3F7",
-    borderRadius: "50px",
+    padding: '2px 4px',
+    display: 'flex',
+    alignItems: 'center',
+    width: '90%',
+    backgroundColor: '#F0F3F7',
+    borderRadius: '50px',
   },
   input: {
     marginLeft: theme.spacing(1),
@@ -39,16 +39,18 @@ const useStyles = makeStyles((theme) => ({
 export default function State() {
   const classes = useStyles();
   const [states, setStates] = useState([]);
-  const [objs,setObjs] = useState({});
-  const [selected, setSelected] = useState("");
+  const [objs, setObjs] = useState([]);
+  const [selected, setSelected] = useState('INDIA');
   const [filteredList, setFilteredList] = useState([]);
+
   const mask = (val) => {
-    console.log("mask called ", val);
+    console.log('mask called ', val);
     let res = states.filter((state) => match(state, val));
     console.log(res);
     console.log(states);
     setFilteredList([...res]);
   };
+
   const match = (state, val) => {
     if (val.length) {
       state = state.toUpperCase();
@@ -66,14 +68,15 @@ export default function State() {
     }
     return true;
   };
+
   useEffect(() => {
-    console.log("new usefeefect");
+    console.log('new usefeefect');
     (async () => {
-      let res = await Axios.get("https://api.rootnet.in/covid19-in/contacts");
+      let res = await Axios.get('https://api.rootnet.in/covid19-in/contacts');
       let data = res.data.data.contacts.regional;
-      console.log("axios res", data);
+      console.log('axios res', data);
       let res2 = await Axios.get(
-        "https://api.rootnet.in/covid19-in/hospitals/beds"
+        'https://api.rootnet.in/covid19-in/hospitals/beds'
       );
       let data2 = res2.data.data.regional;
       console.log(data2);
@@ -89,8 +92,8 @@ export default function State() {
       }
       let temp = [];
 
-      console.log("arr is", arr);
-      console.log("temp is", temp);
+      console.log('arr is', arr);
+      console.log('temp is', temp);
       setObjs(arr);
       //   arr.forEach((item) => console.log(item));
       //   console.log(arr.keys)
@@ -100,56 +103,43 @@ export default function State() {
       setFilteredList(temp);
     })();
   }, []);
-  let obj = {
-    state: "Arunachal Pradesh",
-    ruralHospitals: 3733,
-    ruralBeds: 56721,
-    urbanHospitals: 2312,
-    urbanBeds: 23456,
-    totalHospitals: 23456,
-    totalBeds: 12345654,
-    number: "+91-118289-289",
+  const handleClick = (value) => {
+    console.log(value);
+    setSelected(value);
   };
-  const handleClick = (value) =>{
-      console.log(value);
-      setSelected(value);
-  }
   return (
     <>
       {/* <HeaderCard /> */}
       <Paper
         style={{
-          margin: "20px",
-          borderRadius: "10px",
-          backgroundColor: "#ffffff55",
+          margin: '20px',
+          borderRadius: '10px',
+          backgroundColor: '#ffffff55',
         }}
       >
-        <Grid style={{ padding: "10px" }} container>
-          <Grid item xs={12} md={4} style={{ padding: "20px" }}>
-            <Paper component="form" className={classes.root}>
+        <Grid style={{ padding: '10px' }} container>
+          <Grid item xs={12} md={4} style={{ padding: '20px' }}>
+            <Paper component='form' className={classes.root}>
               <InputBase
                 className={classes.input}
-                placeholder="Search states"
+                placeholder='Search states'
                 onChange={(e) => mask(e.target.value)}
-                inputProps={{ "aria-label": "search google maps" }}
+                inputProps={{ 'aria-label': 'search google maps' }}
               />
               <IconButton
-                type="submit"
+                type='submit'
                 className={classes.iconButton}
-                aria-label="search"
+                aria-label='search'
               >
                 <SearchIcon />
               </IconButton>
-              {/* <Divider className={classes.divider} orientation="vertical" />
-                    <IconButton color="primary" className={classes.iconButton} aria-label="directions">
-                        <DirectionsIcon />
-                    </IconButton> */}
             </Paper>
 
-            <div style={{ height: "300px" }}>
+            <div style={{ height: '300px' }}>
               <StateSelect.Card1
                 states={filteredList}
                 handleClick={handleClick}
+                selected={selected}
               />
             </div>
           </Grid>
@@ -158,8 +148,8 @@ export default function State() {
             item
             xs={12}
             md={8}
-            alignItems="center"
-            justify="center"
+            alignItems='center'
+            justify='center'
           >
             <StateCardNew {...objs[selected]} />
           </Grid>
--- src/components/StateSelect/Card1.js
+++ src/components/StateSelect/Card1.js
@@ -1,29 +1,44 @@
 import { Grid, List, ListItem, ListItemText, Paper } from '@material-ui/core';
 import React from 'react';
 import './index.scss';
-export default function Card1({states,handleClick}) {
-// console.log("inside card1",states);
+export default function Card1({ states, handleClick, selected }) {
+  // console.log("inside card1",states);
   return (
     <>
-    {/* <div style={{}}> */}
-      <List style={{maxWidth:'100%',maxHeight:'100%',overflowY: 'auto'}} dense>
-      {states.map((state=>
-      <ListItem alignItems="center" button key={state} onClick={()=>handleClick(state)}>
-        < ListItemText primary = {
-            < span style = {
-              {
-                fontSize: '17px',
-                fontWeight: 700,
-                color: '#A09CB4'
+      {/* <div style={{}}> */}
+      <List
+        style={{ maxWidth: '100%', maxHeight: '100%', overflowY: 'auto' }}
+        dense
+      >
+        {states.map((state) => (
+          <ListItem
+            alignItems='center'
+            button
+            key={state}
+            onClick={() => handleClick(state)}
+            style={{
+              borderRadius: '30px',
+              backgroundColor:
+                selected === state ? 'rgb(14 36 146)' : 'transparent',
+            }}
+          >
+            <ListItemText
+              primary={
+                <span
+                  style={{
+                    fontSize: '17px',
+                    fontWeight: 700,
+                    color: selected === state ? 'white' : '#A09CB4',
+                  }}
+                >
+                  {state}
+                </span>
               }
-            } >
-          {state}
-        </span>} />
-      </ListItem>
+            />
+          </ListItem>
         ))}
-    </List>
-    {/* </div> */}
-    
+      </List>
+      {/* </div> */}
     </>
   );
 }
